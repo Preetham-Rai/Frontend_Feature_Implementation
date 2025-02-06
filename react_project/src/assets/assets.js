7afde114bdfa8865a66c4850ae6676a5b9d77c53
Preetham-Rai <raipreetham8@gmail.com>
2025-02-06 18:51:12 +0530

    Changes in banner section

---
+++ react_project/src/assets/assets.js
@@ -0,0 +1,7 @@
+import work from "./images/work.jpg";
+import growth from "./images/growth.jpg";
+
+export const assets = {
+  work,
+  growth,
+};
--- react_project/src/components/Banner.jsx
+++ react_project/src/components/Banner.jsx
@@ -1,9 +1,25 @@
+import { assets } from "../assets/assets";
+
 function BannerSection() {
   return (
     <section className="banner-container">
       <div className="banner">
-        <section className="banner-right"></section>
-        <section className="banner-left"></section>
+        <section className="banner-left">
+          <h1>Managing things easy with ManageBox Solution</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+            nulla fugiat aut accusamus cumque obcaecati nihil. Sint adipisci
+            itaque iste? Fugiat molestias cupiditate eum nulla magni possimus,
+            rem ad provident.
+          </p>
+          <div>
+            <button>Explore More</button>
+            <button>Work</button>
+          </div>
+        </section>
+        <section className="banner-right">
+          <img src={assets.work} alt="work image" className="work-image" />
+        </section>
       </div>
     </section>
   );
